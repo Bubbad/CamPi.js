77a7edda24f2ff27f9111e59c25037a149ffe4e3
Johan Nilsson Hansen <granen@hotmail.com>
2014-06-26 00:19:19 +0200

    Added license file for sliders. fixed cpu chart css

--- public/index.js
+++ public/index.js
@@ -61,7 +61,9 @@ function drawChart(cpuData) {
     var options = {
       title: 'CPU usage',
       backgroundColor:"#f5f5f5",
-      chartArea:{left:0,top:61},
+      width:300,
+      height:300,
+      chartArea:{left:0,top:61, height:"100%", width:"100%"},
       colors:["red","blue","yellow"]
     };
 
